b68c1775d63d022cb11fac0e1f215d760e3f0fb2
PI GPU Server <pi@gpu-oa01-001.gensler.ad>
2019-03-28 18:54:19 +0000

    Write each processed video frame to a folder Comment out imshow lines

--- run_video.py
+++ run_video.py
@@ -1,5 +1,7 @@
 import argparse
+import datetime
 import logging
+import os
 import time
 
 import cv2
@@ -16,39 +18,53 @@ formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-fps_time = 0
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
     parser.add_argument('--video', type=str, default='')
     parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
-    parser.add_argument('--show-process', type=bool, default=False,
-                        help='for debug purpose, if enabled, speed for inference is dropped.')
     parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=4.0')
     args = parser.parse_args()
 
     logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
     w, h = model_wh(args.resolution)
     e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
     cap = cv2.VideoCapture(args.video)
+    formatted_date = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+    path = '/opt/cv/result/pose-results/' + formatted_date + '-' + args.model
+    i = 0
+    start_time = time.time()
+
+    # store results here
+    os.makedirs(path)
 
     if cap.isOpened() is False:
         print("Error opening video stream or file")
     while cap.isOpened():
-        ret_val, image = cap.read()
-
-        humans = e.inference(image)
-        if not args.showBG:
-            image = np.zeros(image.shape)
-        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
-
-        cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imshow('tf-pose-estimation result', image)
-        fps_time = time.time()
-        if cv2.waitKey(1) == 27:
+        ret_val, img = cap.read()
+        if ret_val is False:
             break
 
-    cv2.destroyAllWindows()
+        start_fps = time.time()
+        humans = e.inference(img, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+        fps_time = time.time() - start_fps
+        logger.info('fps for frame %i : %f' % (i, 1.0 / fps_time))
+
+        #if not args.showBG:
+        #    image = np.zeros(img.shape)
+
+        image = TfPoseEstimator.draw_humans(img, humans, imgcopy=False)
+
+        #cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        #cv2.imshow('tf-pose-estimation result', image)
+        cv2.imwrite(path + '/' + str(i) + '.jpg', image)
+        i += 1
+        #if cv2.waitKey(1) == 27:
+        #    break
+
+    #cv2.destroyAllWindows()
+    total_time = time.time() - start_time
+    print('Total time for %i frames: %f' % (i, total_time))
 logger.debug('finished+')
